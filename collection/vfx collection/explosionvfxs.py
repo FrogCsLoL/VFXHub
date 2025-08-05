0a84bfd3a96b50e10b40ffac24dc9cdd70513f4f
FrogDivine <lustigleandro@gmail.com>
2025-08-05 08:08:01 +0200

    Update explosionvfxs.py

--- collection/vfx collection/explosionvfxs.py
+++ collection/vfx collection/explosionvfxs.py
@@ -1,44 +1,5 @@
 entries: map[hash,embed] = {
 
-# VFX_HUB_NAME: test2
-# VFX_HUB_DESCRIPTION: test2
-# VFX_HUB_CATEGORY: explosions
-# VFX_HUB_EMITTERS: 6
-    "Aurora_Base_Emote_Dance" = VfxSystemDefinitionData {
-        complexEmitterDefinitionData: list[pointer] = {
-            VfxEmitterDefinitionData {
-                rate: embed = ValueFloat {
-                    constantValue: f32 = 1
-                }
-                particleLifetime: embed = ValueFloat {
-                    constantValue: f32 = 0.200000003
-                }
-                isSingleParticle: flag = true
-                emitterName: string = "Flash"
-                bindWeight: embed = ValueFloat {
-                    constantValue: f32 = 1
-                }
-                particleColorTexture: string = "ASSETS/vfxhub/test2_texture.tex"
-                birthColor: embed = ValueColor {
-                    constantValue: vec4 = { 1, 1, 1, 1 }
-                }
-                pass: i16 = 5
-                meshRenderFlags: u8 = 0
-                miscRenderFlags: u8 = 1
-                isUniformScale: flag = true
-                birthRotation0: embed = ValueVector3 {
-                    constantValue: vec3 = { 0, 0, 0 }
-                }
-                isLocalOrientation: flag = false
-                birthScale0: embed = ValueVector3 {
-                    constantValue: vec3 = { 100, 100, 100 }
-                }
-            }
-        }
-        particleName: string = "Aurora_Base_Emote_Dance"
-        particlePath: string = "Aurora_Base_Emote_Dance"
-    }
-
 # VFX_HUB_NAME: ebay
 # VFX_HUB_DESCRIPTION: ebayname
 # VFX_HUB_CATEGORY: explosions
