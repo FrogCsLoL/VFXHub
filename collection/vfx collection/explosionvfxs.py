2205f833e1138171d06b4b4d0187c027819354ea
FrogDivine <lustigleandro@gmail.com>
2025-08-05 11:51:50 +0200

    Add VFX system: testassetpath3

--- collection/vfx collection/explosionvfxs.py
+++ collection/vfx collection/explosionvfxs.py
@@ -5624,6 +5624,242 @@ entries: map[hash,embed] = {
         soundOnCreateDefault: string = "Play_sfx_Sett_SettW_maxed_oba"
     }
 
+# VFX_HUB_NAME: testassetpath3
+# VFX_HUB_DESCRIPTION: testassetpath3
+# VFX_HUB_CATEGORY: explosions
+# VFX_HUB_EMITTERS: 3
+    "testassetpath3" = VfxSystemDefinitionData {
+        complexEmitterDefinitionData: list[pointer] = {
+            VfxEmitterDefinitionData {
+                rate: embed = ValueFloat {
+                    constantValue: f32 = 1
+                }
+                particleLifetime: embed = ValueFloat {
+                    constantValue: f32 = 0.200000003
+                }
+                lifetime: option[f32] = {
+                    1.5
+                }
+                rateByVelocityFunction: embed = ValueVector2 {
+                    constantValue: vec2 = { 500, 0 }
+                }
+                emitterName: string = "Pantheon"
+                importance: u8 = 2
+                EmitterPosition: embed = ValueVector3 {
+                    dynamics: pointer = VfxAnimatedVector3fVariableData {
+                        probabilityTables: list[pointer] = {
+                            VfxProbabilityTableData {}
+                            VfxProbabilityTableData {}
+                            VfxProbabilityTableData {}
+                        }
+                        times: list[f32] = {
+                            0
+                        }
+                        values: list[vec3] = {
+                            { 0, 0, 0 }
+                        }
+                    }
+                }
+                primitive: pointer = VfxPrimitiveCameraTrail {
+                    mTrail: embed = VfxTrailDefinitionData {
+                        mMode: u8 = 1
+                        mBirthTilingSize: embed = ValueVector3 {
+                            constantValue: vec3 = { 200, 0, 0 }
+                        }
+                    }
+                }
+                birthColor: embed = ValueColor {
+                    constantValue: vec4 = { 0.741176486, 0, 0.109803922, 1 }
+                }
+                color: embed = ValueColor {
+                    dynamics: pointer = VfxAnimatedColorVariableData {
+                        times: list[f32] = {
+                            0
+                            0.0524934381
+                            0.183727041
+                            0.471128613
+                            1
+                        }
+                        values: list[vec4] = {
+                            { 1, 1, 1, 0 }
+                            { 1, 1, 1, 1 }
+                            { 1, 1, 1, 1 }
+                            { 1, 1, 1, 0.216049388 }
+                            { 1, 1, 1, 0 }
+                        }
+                    }
+                }
+                pass: i16 = 100
+                alphaRef: u8 = 0
+                birthScale0: embed = ValueVector3 {
+                    constantValue: vec3 = { 80, 100, 0 }
+                }
+                scale0: embed = ValueVector3 {
+                    dynamics: pointer = VfxAnimatedVector3fVariableData {
+                        times: list[f32] = {
+                            0
+                            1
+                        }
+                        values: list[vec3] = {
+                            { 1, 0, 0 }
+                            { 0, 0, 0 }
+                        }
+                    }
+                }
+                texture: string = "ASSETS/vfxhub/sett_skin45_energy_trail_testassetpath3.tex"
+                birthUvScrollRate: embed = ValueVector2 {
+                    constantValue: vec2 = { 1, 0 }
+                }
+            }
+            VfxEmitterDefinitionData {
+                rate: embed = ValueFloat {
+                    constantValue: f32 = 1
+                }
+                particleLifetime: embed = ValueFloat {
+                    constantValue: f32 = 0.25
+                }
+                particleLinger: option[f32] = {
+                    0.25
+                }
+                lifetime: option[f32] = {
+                    1.5
+                }
+                rateByVelocityFunction: embed = ValueVector2 {
+                    constantValue: vec2 = { 100, 0 }
+                }
+                emitterName: string = "Pantheon1"
+                importance: u8 = 2
+                primitive: pointer = VfxPrimitiveCameraTrail {
+                    mTrail: embed = VfxTrailDefinitionData {
+                        mMode: u8 = 1
+                        mBirthTilingSize: embed = ValueVector3 {
+                            constantValue: vec3 = { 700, 0, 0 }
+                        }
+                        mSmoothingMode: u8 = 2
+                    }
+                }
+                blendMode: u8 = 1
+                birthColor: embed = ValueColor {
+                    constantValue: vec4 = { 0.0392156877, 0.0392156877, 0.0392156877, 1 }
+                }
+                color: embed = ValueColor {
+                    dynamics: pointer = VfxAnimatedColorVariableData {
+                        times: list[f32] = {
+                            0
+                            0.586614192
+                            1
+                        }
+                        values: list[vec4] = {
+                            { 1, 1, 1, 1 }
+                            { 1, 1, 1, 0.489997715 }
+                            { 1, 1, 1, 0 }
+                        }
+                    }
+                }
+                alphaRef: u8 = 0
+                birthScale0: embed = ValueVector3 {
+                    constantValue: vec3 = { 120, 100, 0 }
+                }
+                scale0: embed = ValueVector3 {
+                    dynamics: pointer = VfxAnimatedVector3fVariableData {
+                        times: list[f32] = {
+                            0
+                            0.60104984
+                            1
+                        }
+                        values: list[vec3] = {
+                            { 1, 0, 0 }
+                            { 0.19421488, 0.60104984, 0.60104984 }
+                            { 0, 1, 1 }
+                        }
+                    }
+                }
+                texture: string = "ASSETS/vfxhub/sett_skin45_fire_trail_graphic_testassetpath3.tex"
+                birthUvScrollRate: embed = ValueVector2 {
+                    constantValue: vec2 = { 0, -0.5 }
+                }
+            }
+            VfxEmitterDefinitionData {
+                rate: embed = ValueFloat {
+                    constantValue: f32 = 1
+                }
+                particleLifetime: embed = ValueFloat {
+                    constantValue: f32 = 0.25
+                }
+                particleLinger: option[f32] = {
+                    0.25
+                }
+                lifetime: option[f32] = {
+                    1.5
+                }
+                rateByVelocityFunction: embed = ValueVector2 {
+                    constantValue: vec2 = { 100, 0 }
+                }
+                emitterName: string = "Pantheon2"
+                importance: u8 = 2
+                primitive: pointer = VfxPrimitiveCameraTrail {
+                    mTrail: embed = VfxTrailDefinitionData {
+                        mMode: u8 = 1
+                        mBirthTilingSize: embed = ValueVector3 {
+                            constantValue: vec3 = { 700, 0, 0 }
+                        }
+                        mSmoothingMode: u8 = 2
+                    }
+                }
+                blendMode: u8 = 1
+                birthColor: embed = ValueColor {
+                    constantValue: vec4 = { 0.0392156877, 0.0392156877, 0.0392156877, 1 }
+                }
+                color: embed = ValueColor {
+                    dynamics: pointer = VfxAnimatedColorVariableData {
+                        times: list[f32] = {
+                            0
+                            0.100000001
+                            0.300000012
+                            0.586614192
+                            0.600000024
+                            1
+                        }
+                        values: list[vec4] = {
+                            { 0.0392156877, 0.0392156877, 0.0392156877, 0 }
+                            { 0.0392156877, 0.0392156877, 0.0392156877, 1 }
+                            { 0.0392156877, 0.0392156877, 0.0392156877, 1 }
+                            { 0.0392156877, 0.0392156877, 0.0392156877, 0.90196079 }
+                            { 0.0392156877, 0.0392156877, 0.0392156877, 0.772549033 }
+                            { 0.0392156877, 0.0392156877, 0.0392156877, 0 }
+                        }
+                    }
+                }
+                pass: i16 = 1
+                alphaRef: u8 = 0
+                birthScale0: embed = ValueVector3 {
+                    constantValue: vec3 = { 100, 100, 0 }
+                }
+                scale0: embed = ValueVector3 {
+                    dynamics: pointer = VfxAnimatedVector3fVariableData {
+                        times: list[f32] = {
+                            0
+                            0.60104984
+                            1
+                        }
+                        values: list[vec3] = {
+                            { 1, 0, 0 }
+                            { 0.19421488, 0.60104984, 0.60104984 }
+                            { 0, 1, 1 }
+                        }
+                    }
+                }
+                texture: string = "ASSETS/vfxhub/sett_skin45_fire_trail_graphic_testassetpath3.tex"
+                birthUvScrollRate: embed = ValueVector2 {
+                    constantValue: vec2 = { 0, -0.5 }
+                }
+            }
+        }
+        particleName: string = "testassetpath3"
+        particlePath: string = "testassetpath3"
+    }
+
+
 
 
 
@@ -5653,6 +5889,7 @@ entries: map[hash,embed] = {
             "isitfixed" = "isitfixed"
             "wegotesta" = "wegotesta"
             "testassetpath2" = "testassetpath2"
+            "testassetpath3" = "testassetpath3"
         }
      }
 } 
