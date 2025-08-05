49a2a719d13a65138838ea28225790499f4108d2
FrogDivine <lustigleandro@gmail.com>
2025-08-05 04:58:36 +0200

    Add VFX system: test

--- collection/vfx collection/explosionvfxs.py
+++ collection/vfx collection/explosionvfxs.py
@@ -1,5 +1,19 @@
 entries: map[hash,embed] = {
 
+# VFX_HUB_NAME: test
+# VFX_HUB_DESCRIPTION: test
+# VFX_HUB_CATEGORY: explosions
+# VFX_HUB_EMITTERS: 6
+    "Aurora_Base_BA_tar" = VfxSystemDefinitionData {
+        complexEmitterDefinitionData: list[pointer] = {
+            VfxEmitterDefinitionData {
+                rate: embed = ValueFloat {
+                    constantValue: f32 = 1
+                    dynamics: pointer = VfxAnimatedFloatVariableData {
+                        probabilityTables: list[pointer] = {
+                            VfxProbabilityTableData {}
+
+
 # VFX_HUB_NAME: auroraexplison
 # VFX_HUB_DESCRIPTION: auroraexplison
 # VFX_HUB_CATEGORY: explosions
