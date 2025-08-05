2f7dff96a9627ca950cb669298d6b53e5fcc3f2e
FrogDivine <lustigleandro@gmail.com>
2025-08-05 05:13:14 +0200

    Add VFX system: aaa

--- collection/vfx collection/explosionvfxs.py
+++ collection/vfx collection/explosionvfxs.py
@@ -1,5 +1,18 @@
 entries: map[hash,embed] = {
 
+# VFX_HUB_NAME: aaa
+# VFX_HUB_DESCRIPTION: aaa
+# VFX_HUB_CATEGORY: explosions
+# VFX_HUB_EMITTERS: 15
+    "Aurora_Base_R_AoERingRecast" = VfxSystemDefinitionData {
+        complexEmitterDefinitionData: list[pointer] = {
+            VfxEmitterDefinitionData {
+                rate: embed = ValueFloat {
+                    constantValue: f32 = 50
+                }
+               
+
+
 # VFX_HUB_NAME: test2
 # VFX_HUB_DESCRIPTION: test2
 # VFX_HUB_CATEGORY: explosions
