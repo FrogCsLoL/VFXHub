a89bee02308e098c1ffd1b7ded42a13ed905b6de
FrogDivine <lustigleandro@gmail.com>
2025-08-05 04:46:15 +0200

    Create explosionvfxs.py

---
+++ collection/vfx collection/explosionvfxs.py
@@ -0,0 +1,14 @@
+entries: map[hash,embed] = {
+    "insertvfxsystemname" = VfxSystemDefinitionData {
+        complexEmitterDefinitionData: list[pointer] = {
+            #insertvfxemitterdefinitiondatahere
+        }
+        particleName: string = "insertvfxsystemname"
+        particlePath: string = "insertvfxsystemname"
+    }
+    "Characters/Aurora/Skins/Skin0/Resources" = ResourceResolver {
+        resourceMap: map[hash,link] = {
+            "insertvfxsystemname" = "insertvfxsystemname"
+        }
+    }
+}
