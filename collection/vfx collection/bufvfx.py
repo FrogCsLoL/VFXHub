f21d569945c7d910245538b637a460b22986cc8d
FrogDivine <lustigleandro@gmail.com>
2025-10-17 04:07:41 +0200

    Add VFX system: Signature

--- collection/vfx collection/bufvfx.py
+++ collection/vfx collection/bufvfx.py
@@ -8265,6 +8265,64 @@ mEyeCandy: bool = true
         }
     }
 
+# VFX_HUB_NAME: Signature
+# VFX_HUB_DESCRIPTION: Signature
+# VFX_HUB_CATEGORY: auras
+# VFX_HUB_EMITTERS: 1
+    "Signature" = VfxSystemDefinitionData {
+        complexEmitterDefinitionData: list[pointer] = {
+            VfxEmitterDefinitionData {
+                timeBeforeFirstEmission: f32 = 0.1
+                rate: embed = ValueFloat {
+                    constantValue: f32 = 1
+                }
+                particleLifetime: embed = ValueFloat {
+                    constantValue: f32 = 10
+                }
+                isSingleParticle: flag = true
+                emitterName: string = "Signature"
+                0x563d4a22: embed = ValueVector3 {
+                    constantValue: vec3 = { 15, 0, -200 }
+                }
+                primitive: pointer = VfxPrimitiveArbitraryQuad {}
+                blendMode: u8 = 1
+                pass: i16 = 500
+                miscRenderFlags: u8 = 1
+                isGroundLayer: flag = true
+                useNavmeshMask: flag = true
+                birthRotation0: embed = ValueVector3 {
+                    constantValue: vec3 = { 90, 90, 5 }
+                }
+                isLocalOrientation: flag = false
+                birthScale0: embed = ValueVector3 {
+                    constantValue: vec3 = { -100, 130, 0 }
+                }
+                scale0: embed = ValueVector3 {
+                    dynamics: pointer = VfxAnimatedVector3fVariableData {
+                        times: list[f32] = {
+                            0
+                            0.0179999992
+                        }
+                        values: list[vec3] = {
+                            { 1, 0, 0.400000006 }
+                            { 1, 1, 1 }
+                        }
+                    }
+                }
+                texture: string = "ASSETS/vfxhub/Vayne_Skin15_LWX_Signature.dds"
+            }
+        }
+        particleName: string = "Signature"
+        particlePath: string = "Signature"
+        transform: mtx44 = {
+            1.10000002, 0, 0, 0
+            0, 1.10000002, 0, 0
+            0, 0, 1.10000002, 0
+            -5, 0, 0, 1
+        }
+    }
+
+
 
 
 
@@ -8275,6 +8333,7 @@ mEyeCandy: bool = true
             "caitlyncam" = "caitlyncam"
             "testebay" = "testebay"
             "Challenger Recall" = "Challenger Recall"
+            "Signature" = "Signature"
         }
      }
 } 
